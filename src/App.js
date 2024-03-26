521d796372dea840959370b7c9667d26c90b33a3
lalitha1900 <trlalitha2000@gmail.com>
2024-03-26 23:21:07 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cites, setCites] = useState([]);
+  const [selectedcountries, setSelectedCountries] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState([]);
+  const [selectedcountry, setSelectedCountry] = useState("");
+  getCountries = async function () {
+    var response = await fetch(
+      " https://crio-location-selector.onrender.com/countries"
+    );
+    var data = await response.json();
+    setCountries(data);
+    console.log(data);
+  };
 
-function App() {
+  async function onChangeCountry(e) {
+    console.log("check" + e.target.value);
+    setSelectedCountry(e.target.value);
+    setSelectedState("");
+    setSelectedCity("");
+    await getStates(e.target.value);
+  }
+  getStates = async function (country) {
+    var response = await fetch(
+      "https://crio-location-selector.onrender.com/country=" +
+        country +
+        "/states"
+    );
+    if (response.ok) {
+      var data = await response.json();
+      setStates(data);
+      document
+        .getElementsByClassName("stateClass")[0]
+        .removeAttribute("disabled");
+      console.log(data);
+    }
+  };
+
+  async function onChangeState(e) {
+    console.log("check");
+    setSelectedState(e.target.value);
+    setSelectedCity("");
+    await getCites(e.target.value);
+  }
+
+  getCites = async function (stateName) {
+    var response = await fetch(
+      "https://crio-location-selector.onrender.com/country=" +
+        selectedcountry +
+        "/state=" +
+        stateName +
+        "/cities"
+    );
+    if (response.ok) {
+      var data = await response.json();
+      setCites(data);
+      document
+        .getElementsByClassName("cityClass")[0]
+        .removeAttribute("disabled");
+      console.log(data);
+    }
+  };
+  useEffect(() => {
+    getCountries();
+  }, [selectedcountries]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="city-selector">
+      <h1>Select Location</h1>
+      <div className="dropdowns">
+        <select
+          value={selectedcountry}
+          onChange={(e) => onChangeCountry(e)}
+          className="dropdown"
         >
-          Learn React
-        </a>
-      </header>
+          <option value="" disabled>
+            Select Country
+          </option>
+          {countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+        <select
+          value={selectedState}
+          onChange={(e) => onChangeState(e)}
+          className="dropdown stateClass"
+          disabled
+        >
+          <option value="" disabled>
+            Select State
+          </option>
+          {selectedcountry &&
+            states.map((country) => (
+              <option key={country} value={country}>
+                {country}
+              </option>
+            ))}
+        </select>
+        <select
+          value={selectedCity}
+          onChange={(e) => setSelectedCity(e.target.value)}
+          className="dropdown cityClass"
+          disabled
+        >
+          <option value="" disabled>
+            Select City
+          </option>
+          {cites.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
-
-export default App;
